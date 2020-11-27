681790e0b12c6fcd72743f426dde05ca96470a77
Alfath Dioni <fathalfath30@gmail.com>
2020-11-28 01:22:16 +0700

    adding config

---
+++ config/app.js
@@ -0,0 +1,26 @@
+/*
+//
+//  ______    _   _           _  __      _   _     ____   ___
+// |  ____|  | | | |         | |/ _|    | | | |   |___ \ / _ \
+// | |__ __ _| |_| |__   __ _| | |_ __ _| |_| |__   __) | | | |
+// |  __/ _` | __| '_ \ / _` | |  _/ _` | __| '_ \ |__ <| | | |
+// | | | (_| | |_| | | | (_| | | || (_| | |_| | | |___) | |_| |
+// |_|  \__,_|\__|_| |_|\__,_|_|_| \__,_|\__|_| |_|____/ \___/
+//
+// Written by Fathalfath30.
+// Email : fathalfath30@gmail.com
+// Follow me on:
+//  Github : https://github.com/fathalfath30
+//  Gitlab : https://gitlab.com/Fathalfath30
+//
+*/
+module.exports = {
+  listeningPort: 3000,
+  system: {
+    // eslint-disable-next-line max-len
+    secret: 'a1b6bf82590ba4b1e3934ba37b8a865057f6fdf5424a976e8d20448a8690398102b4a48d6b4265552ea0e16512e5a5a7a88def7922c2c7686f95c536c7f57204',
+  },
+  encryption: {
+    saltRounds: 10,
+  },
+}
