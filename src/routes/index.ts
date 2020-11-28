9e53a3032918268b7c4be4c07909c89b467183dc
Alfath Dioni <fathalfath30@gmail.com>
2020-11-28 15:34:23 +0700

    wip: api routing

---
+++ src/routes/index.ts
@@ -0,0 +1,43 @@
+/*
+//
+//  ______    _   _           _  __      _   _     ____   ___
+// |  ____|  | | | |         | |/ _|    | | | |   |___ \ / _ \
+// | |__ __ _| |_| |__   __ _| | |_ __ _| |_| |__   __) | | | |
+// |  __/ _` | __| '_ \ / _` | |  _/ _` | __| '_ \ |__ <| | | |
+// | | | (_| | |_| | | | (_| | | || (_| | |_| | | |___) | |_| |
+// |_|  \__,_|\__|_| |_|\__,_|_|_| \__,_|\__|_| |_|____/ \___/
+//
+// Written by Fathalfath30.
+// Email : fathalfath30@gmail.com
+// Follow me on:
+//  Github : https://github.com/fathalfath30
+//  Gitlab : https://gitlab.com/Fathalfath30
+//
+*/
+import express, {Request, Response, NextFunction} from "express";
+import {response} from '../Modules/Response'
+
+class RtchatServerRoutes {
+  private r = express.Router()
+
+  public Routes() {
+    this.r.use((req: Request, res: Response, next: NextFunction) => {
+      console.log(`Something happening! [${req.headers['x-forwarded-for'] || req.connection.remoteAddress}]`)
+
+      next()
+    })
+
+    this.r.get('/', (req: Request, res: Response, next: NextFunction) => {
+      response(res, {}, 200, "It's works!")
+    })
+
+    this.r.use('/api', () => {
+
+    })
+
+    return this.r
+  }
+}
+
+const BaseRoutes = new RtchatServerRoutes();
+export {BaseRoutes}
