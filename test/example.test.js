e6523dbe937a7beeb053ec0783c5972b0005caa6
Alfath Dioni <fathalfath30@gmail.com>
2020-12-02 12:57:08 +0700

    adding unit test (example)'

---
+++ test/example.test.js
@@ -0,0 +1,26 @@
+/*
+//
+//  ______    _   _           _  __      _   _     ____   ___
+// |  ____|  | | | |         | |/ _|    | | | |   |___ \ / _ \
+// | |__ __ _| |_| |__   __ _| | |_ __ _| |_| |__   __) | | | |
+// |  __/ _` | __| '_ \ / _` | |  _/ _` | __| '_ \ |__ <| | | |
+// | | | (_| | |_| | | | (_| | | || (_| | |_| | | |___) | |_| |
+// |_|  \__,_|\__|_| |_|\__,_|_|_| \__,_|\__|_| |_|____/ \___/
+//
+// Written by Fathalfath30.
+// Email : fathalfath30@gmail.com
+// Follow me on:
+//  Github : https://github.com/fathalfath30
+//  Gitlab : https://gitlab.com/Fathalfath30
+//
+*/
+const assert = require ('assert')
+describe ('Simple Math Test', () => {
+  it ('should return 2', () => {
+    assert.equal (1 + 1, 2)
+  })
+
+  it ('should return 9', () => {
+    assert.equal (3 * 3, 9)
+  })
+})
