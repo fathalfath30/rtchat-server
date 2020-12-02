790d79b2ee3c12b85f717dc8e669bc8b41bb92b8
Alfath Dioni <fathalfath30@gmail.com>
2020-12-02 12:49:22 +0700

    adding signature

--- migrations/20201201172627-user_roles.js
+++ migrations/20201201172627-user_roles.js
@@ -1,5 +1,22 @@
-'use strict';
+/*
+//
+//  ______    _   _           _  __      _   _     ____   ___
+// |  ____|  | | | |         | |/ _|    | | | |   |___ \ / _ \
+// | |__ __ _| |_| |__   __ _| | |_ __ _| |_| |__   __) | | | |
+// |  __/ _` | __| '_ \ / _` | |  _/ _` | __| '_ \ |__ <| | | |
+// | | | (_| | |_| | | | (_| | | || (_| | |_| | | |___) | |_| |
+// |_|  \__,_|\__|_| |_|\__,_|_|_| \__,_|\__|_| |_|____/ \___/
+//
+// Written by Fathalfath30.
+// Email : fathalfath30@gmail.com
+// Follow me on:
+//  Github : https://github.com/fathalfath30
+//  Gitlab : https://gitlab.com/Fathalfath30
+//
+*/
+'use strict'
 
+const tableName = ''
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     /**
@@ -17,5 +34,5 @@ module.exports = {
      * Example:
      * await queryInterface.dropTable('users');
      */
-  }
-};
+  },
+}
